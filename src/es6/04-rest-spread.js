0cecc3c1801b10110cded6e1ded251b03ded29be
omarwin21 <omar.zambrano@udea.edu.co>
2022-11-10 20:56:57 -0500

    asignación de desestructuración

---
+++ src/es6/04-rest-spread.js
@@ -0,0 +1,11 @@
+    // arrays destructuring
+
+    let fruits = ['Apple', 'Banana'];
+    let [a, b] = fruits;
+    console.log(a, b);
+
+// Object destructuring
+
+let user = {username: 'Omar', age: 29};
+let { username, age } = user;
+console.log(username, age);
