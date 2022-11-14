2bc9d1764e650b1ac9e172fd70949bcfce024d7b
omarwin21 <omar.zambrano@udea.edu.co>
2022-11-14 09:30:41 -0500

    Spread operator

--- src/es6/04-rest-spread.js
+++ src/es6/04-rest-spread.js
@@ -17,3 +17,13 @@ let country = 'CO';
 
 let data = {id: 1,  ...person, country }
 console.log(data); 
+
+//rest
+
+function sum(num, ...values){
+    console.log(values);
+    console.log(num + values[0]);
+    return num+values[0];
+}
+
+sum(1, 1, 2, 3);
