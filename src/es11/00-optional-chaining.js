a420c8f180b999f6639535af67b0c1bd46c9cc51
omarwin21 <omar.zambrano@udea.edu.co>
2022-11-20 09:00:22 -0500

    ES11

---
+++ src/es11/00-optional-chaining.js
@@ -0,0 +1,10 @@
+const users = {
+    omlnx : {
+        country: 'CO'
+    },
+    ana: {
+        country: 'MX'
+    }
+}
+
+console.log(users?.bebeloper?.country)
---
+++ src/es11/01-bigint.js
@@ -0,0 +1,4 @@
+const aBigNumber = 56446565321684661n;
+const anotherBigNumber = BigInt(56446565321684661);
+console.log(aBigNumber)
+console.log(anotherBigNumber)
